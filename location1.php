601856db54a184bd01f7ff789e569cd5eab7f6f9
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-11-15 19:38:04 +0900

    redirect.php

---
+++ location1.php
@@ -0,0 +1,8 @@
+
+<!-- It is redirected when it is requested -->
+<!-- 302 is Temporary move -->
+<?php
+  header('Location: http://localhost/php/RequestHeader.php');
+  // 301 is Permanent move
+  // header('Location: http://www.example.com/', true, 301);
+ ?>
